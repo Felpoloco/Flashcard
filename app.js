4cee118bee145744ca79823f36b9389b104ca1af
Felpszz <00001118334668sp@al.educacao.sp.gov.br>
2024-11-19 01:02:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-
+//Felipe da Silva Santos 3ºD 19/11/2024
     cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
